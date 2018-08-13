5487e3bae2104ed3e8dd6b13893096fe203026af
m-e-h <info@martyhelmick.com>
2018-08-13 16:20:17 -0400

    More deliberate find/replace and reorg for extending.

---
+++ cli.js
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+"use strict";
+
+const path = require("path");
+const meow = require("meow");
+const themeClaim = require("./lib/theme-claim");
+const getConf = require("./lib/configPath");
+
+const themeRoot = getConf.themeRoot;
+
+const cli = meow(
+	`
+	Usage
+	  $ theme-claim --config=<path>
+
+	Options
+	  --config=<path>  Path to config [Default: ./themeclaim.json]
+
+	Examples
+	  $ theme-claim --config='/build/conf.json'
+`,
+	{
+		flags: {
+			config: {
+				type: "string",
+				alias: "c",
+				default: getConf()
+			}
+		}
+	}
+);
+
+const pathToConf = path.resolve(themeRoot, cli.flags.config);
+
+//console.log(pathToConf);
+(async () => {
+	await themeClaim(pathToConf);
+})();
